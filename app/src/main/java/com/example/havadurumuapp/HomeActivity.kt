b6884df1bdb1f8380a4cf9781a26d0c9e08aa4c1
osmnkiraz <45825918+osmnkiraz@users.noreply.github.com>
2020-01-15 16:37:38 +0300

    15.01.20 home animasyonlu

--- app/src/main/java/com/example/havadurumuapp/HomeActivity.kt
+++ app/src/main/java/com/example/havadurumuapp/HomeActivity.kt
@@ -4,13 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+
+    var uptodowninfinite: Animation?=null
+    var doksanderecedonme: Animation?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        uptodowninfinite= AnimationUtils.loadAnimation(applicationContext,R.anim.downtoupinfinity)
+        doksanderecedonme= AnimationUtils.loadAnimation(applicationContext,R.anim.doksanrotate)
+        imgSemsiye.animation=uptodowninfinite
+        imgExchange.animation=doksanderecedonme
+
 
         tvWeather.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
